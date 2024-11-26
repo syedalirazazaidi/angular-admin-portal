349a00ab9fecc2d7ecc3236deb057fc1a74633d9
Ali Raza <aaliraza007@hotmail.com>
2024-11-26 14:23:22 +0500

    add new compoonent

--- src/app/components/childregistration/childregistration.component.ts
+++ src/app/components/childregistration/childregistration.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {  heroLockOpen } from '@ng-icons/heroicons/outline';
 @Component({
   selector: 'app-childregistration',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
+  viewProviders: [provideIcons({ heroLockOpen })],
+
   templateUrl: './childregistration.component.html',
-  styleUrl: './childregistration.component.css'
+  styleUrl: './childregistration.component.css',
 })
 export class ChildregistrationComponent {
-
+  childId: string = '';
+  generatePassword(): void {
+    this.childId = Array(12)
+      .fill(null)
+      .map(() =>
+        Math.random()
+          .toString(36)
+          .charAt(Math.random() < 0.5 ? 2 : 9)
+      )
+      .join('');
+  }
 }
